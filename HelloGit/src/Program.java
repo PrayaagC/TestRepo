3abcc81dd5b881f3ce96ca91fc5a3d6cf4f62526
s-pcalmidi <s-pcalmidi@484E302302.lwsd.k12wa.org>
2024-05-30 11:24:58 -0700

    Added exclamation

--- HelloGit/src/Program.java
+++ HelloGit/src/Program.java
@@ -3,7 +3,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello Git");
+		System.out.print("Hello Git!");
 	}
 
 }
